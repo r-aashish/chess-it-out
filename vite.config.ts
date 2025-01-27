51741ac4195411bc95264e1f24e1392b637db262
Aashish Reddy <aashish@Aashishs-MacBook-Air-2.local>
2025-01-27 17:34:11 -0500

    Fix image loading issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'images', dest: 'dist' },
+      ],
+      hook: 'writeBundle'
+    })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
