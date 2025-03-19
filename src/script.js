fd7fb2301a67cfebf8fc18813321475fd7040234
Aashish Reddy <61154281+r-aashish@users.noreply.github.com>
2025-03-19 12:17:15 -0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -130,15 +130,6 @@
                     src: "images/among-us-pieces/6.png",
                     width: 100,
                     height: 100
-                  },
-                  {
-                    src: "images/among-us-pieces/7.png",
-                    width: 100,
-                    height: 100
-                  },{
-                    src: "images/among-us-pieces/8.png",
-                    width: 100,
-                    height: 100
                   }
                 ]
               }
